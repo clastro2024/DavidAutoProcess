4aea1d14ee79f5a628dc37b3d3551247cfa92b9c
LAPTOP-VUJG7L0C\s_sai <s_sain@hotmail.com>
2024-11-25 10:35:12 +0530

    Worked by Sukhendu

--- Codes/config.py
+++ Codes/config.py
@@ -6,8 +6,8 @@
 
 
 ## Files Path ##
-AKINS_FOMO_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\Python-Files\Data&Files\AKINS FoMoCo_Piece_Sales_112222_YTD.xlsx"
-GPARTS_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\Python-Files\Data&Files\GPARTS Part Measures.xlsx"
-WHOLESALE_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\Python-Files\Data&Files\Wholesale JAN_Oct_Parts_Ranking_Counter_Invoices_All_Brands.xlsx"
-SERVICE_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\Python-Files\Data&Files\Service JAN_Oct_Parts_Ranking_ROs_All_Brands.xlsx"
-COUNTERPAD_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\Python-Files\Data&Files\Counter_Pad_11142024.xlsx"
+AKINS_FOMO_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\StorageRackOptimization\Data&Files\AKINS FoMoCo_Piece_Sales_112222_YTD.xlsx"
+GPARTS_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\StorageRackOptimization\Data&Files\GPARTS Part Measures.xlsx"
+WHOLESALE_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\StorageRackOptimization\Data&Files\Wholesale JAN_Oct_Parts_Ranking_Counter_Invoices_All_Brands.xlsx"
+SERVICE_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\StorageRackOptimization\Data&Files\Service JAN_Oct_Parts_Ranking_ROs_All_Brands.xlsx"
+COUNTERPAD_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\StorageRackOptimization\Data&Files\Counter_Pad_11142024.xlsx"
