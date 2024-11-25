b2256802d59e2ec266c537f50c9e92eeea975f13
LAPTOP-VUJG7L0C\s_sai <s_sain@hotmail.com>
2024-11-25 18:00:37 +0530

    Removed notebook

--- Codes/config.py
+++ Codes/config.py
@@ -1,12 +1,14 @@
 ################################  config.py  ####################################
 # Author: Sukhendu Sain
 # Description: Config file of codebase. Houses different parameters and variables
-# Data: 22-Nov-2024
+# Data: 23-Nov-2024
 #################################################################################
-
+import os
 
 ## Files Path ##
-AKINS_FOMO_FILE_PATH = r"\StorageRackOptimization\Data&Files\AKINS FoMoCo_Piece_Sales_112222_YTD.xlsx"
+ROOT_PATH = '\\'.join(os.getcwd().split('\\')[:-1])
+print(ROOT_PATH)
+AKINS_FOMO_FILE_PATH = r"D:\Optimization\Data&Files\AKINS FoMoCo_Piece_Sales_112222_YTD.xlsx"
 GPARTS_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\StorageRackOptimization\Data&Files\GPARTS Part Measures.xlsx"
 WHOLESALE_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\StorageRackOptimization\Data&Files\Wholesale JAN_Oct_Parts_Ranking_Counter_Invoices_All_Brands.xlsx"
 SERVICE_FILE_PATH = r"C:\Sukhendu\UPWORK-WORK\0_Dondray_Auto\StorageRackOptimization\Data&Files\Service JAN_Oct_Parts_Ranking_ROs_All_Brands.xlsx"
